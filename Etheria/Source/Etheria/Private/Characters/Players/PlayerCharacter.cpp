5451b101393997d488c3925f9b5c5ca0b4481c7a
0nnen <onnenennon@gmail.com>
2025-11-05 11:47:46 +0100

    [+] Add combat and lock-on systems to PlayerCharacter
    Introduced CombatComponent and LockTargetComponent to PlayerCharacter:
    - With corresponding input actions and handler methods for light/heavy attacks, parry, dodge, and target locking.
    - Refactored input binding and handler organization for clarity, and updated header to declare new components and input actions.

--- Etheria/Source/Etheria/Private/Characters/Players/PlayerCharacter.cpp
+++ Etheria/Source/Etheria/Private/Characters/Players/PlayerCharacter.cpp
@@ -1,12 +1,11 @@
 /**
  * Etheria's End Project, 2025
  * Created by: Zhailendra
- * Last Updated by: Zhailendra
+ * Last Updated by: 0nnen
  * Class: PlayerCharacter - Source
  */
 
 #include "Characters/Players/PlayerCharacter.h"
-
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
@@ -15,6 +14,8 @@
 #include "Components/Characters/Player/Glider/GliderComponent.h"
 #include "Components/Interaction/InteractorComponent.h"
 #include "Components/Inventory/InventoryComponent.h"
+#include "Components/Combat/CombatComponent.h"
+#include "Components/Combat/LockTargetComponent.h"
 
 APlayerCharacter::APlayerCharacter()
 {
@@ -37,15 +38,22 @@ APlayerCharacter::APlayerCharacter()
 
     GetCharacterMovement()->bOrientRotationToMovement = true;
 
-    // --- GLIDER ---
+    // --- GLIDER COMPONENT ---
     GliderComponent = CreateDefaultSubobject<UGliderComponent>(TEXT("GliderComponent"));
     GliderVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GliderVisual"));
     GliderVisual->SetupAttachment(RootComponent);
 
-    // --- PLAYER COMPONENTS ---
-    
+    // --- INVENTORY COMPONENT ---
     InventoryComponent  = CreateDefaultSubobject<UInventoryComponent>(TEXT("BPC_Inventory"));
+
+    // --- INTERACTOR COMPONENT ---
     InteractorComponent = CreateDefaultSubobject<UInteractorComponent>(TEXT("BPC_Interactor"));
+
+    // --- COMBAT COMPONENT ---
+    CombatComponent     = CreateDefaultSubobject<UCombatComponent>(TEXT("BPC_Combat"));
+    
+    // --- LOCK COMPONENT ---
+    LockTargetComponent = CreateDefaultSubobject<ULockTargetComponent>(TEXT("BPC_LockTarget"));
 }
 
 void APlayerCharacter::BeginPlay()
@@ -93,7 +101,13 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
 
     if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
     {
-        // Mouvement 4 touches
+    #pragma region "CAMERA BINDS"
+        // CAMERA BINDS
+        EIC->BindAction(LookAction,  ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
+    #pragma endregion
+        
+    #pragma region "MOVEMENTS BINDS"
+        // MOVEMENTS BINDS
         EIC->BindAction(ForwardAction, ETriggerEvent::Started,   this, &APlayerCharacter::OnForwardStarted);
         EIC->BindAction(ForwardAction, ETriggerEvent::Completed, this, &APlayerCharacter::OnForwardCompleted);
         EIC->BindAction(BackAction,    ETriggerEvent::Started,   this, &APlayerCharacter::OnBackStarted);
@@ -104,32 +118,83 @@ void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCom
         EIC->BindAction(RightAction, ETriggerEvent::Started,   this, &APlayerCharacter::OnRightStarted);
         EIC->BindAction(RightAction, ETriggerEvent::Completed, this, &APlayerCharacter::OnRightCompleted);
 
-        // Autres actions
-        EIC->BindAction(LookAction,  ETriggerEvent::Triggered, this, &APlayerCharacter::Look);
         EIC->BindAction(JumpAction,  ETriggerEvent::Triggered, this, &ACharacter::Jump);
         EIC->BindAction(CrouchAction, ETriggerEvent::Started,   this, &APlayerCharacter::StartCrouch);
         EIC->BindAction(CrouchAction, ETriggerEvent::Completed, this, &APlayerCharacter::StopCrouch);
         EIC->BindAction(SprintAction, ETriggerEvent::Started,   this, &APlayerCharacter::StartSprint);
         EIC->BindAction(SprintAction, ETriggerEvent::Completed, this, &APlayerCharacter::StopSprint);
+    #pragma endregion
+
+    #pragma region "GLIDER BINDS"
+        // GLIDER BINDS
         EIC->BindAction(GliderAction, ETriggerEvent::Started,   this, &APlayerCharacter::ToggleGlideMode);
         EIC->BindAction(DiveAction,   ETriggerEvent::Started,   this, &APlayerCharacter::ToggleDiveMode);
+    #pragma endregion
+
+    #pragma region "INVENTORY BINDS"
+        // INVENTORY BINDS
         EIC->BindAction(NextItemAction, ETriggerEvent::Triggered, this, &ThisClass::Input_SelectNext);
         EIC->BindAction(PrevItemAction, ETriggerEvent::Triggered, this, &ThisClass::Input_SelectPrev);
         EIC->BindAction(UseItemAction,  ETriggerEvent::Started, this, &ThisClass::Input_UseItem);
         EIC->BindAction(DropItemAction, ETriggerEvent::Started, this, &ThisClass::Input_DropItem);
+    #pragma endregion
+
+    #pragma region "INTERACTION BINDS"
+        // INTERACTION BIND
         EIC->BindAction(InteractAction, ETriggerEvent::Started, this, &ThisClass::Input_Interact);
+    #pragma endregion
+
+    #pragma region "COMBAT BINDS"
+        // COMBAT BINDS
+        if (AttackLightAction)
+        {
+            // Press: start or chain combo
+            EIC->BindAction(AttackLightAction, ETriggerEvent::Started,   this, &APlayerCharacter::OnAttackLightPressed);
+            // Release: can be used to trigger buffered advance as you prefer
+            EIC->BindAction(AttackLightAction, ETriggerEvent::Completed, this, &APlayerCharacter::OnAttackLightReleased);
+        }
+
+        if (AttackHeavyAction)
+        {
+            // Press: start a charge-capable attack (heavy id)
+            EIC->BindAction(AttackHeavyAction, ETriggerEvent::Started,   this, &APlayerCharacter::OnAttackHeavyPressed);
+            // Release: release the charge at current level
+            EIC->BindAction(AttackHeavyAction, ETriggerEvent::Completed, this, &APlayerCharacter::OnAttackHeavyReleased);
+            // Canceled path if your input can be canceled
+            EIC->BindAction(AttackHeavyAction, ETriggerEvent::Canceled,  this, &APlayerCharacter::OnAttackHeavyCanceled);
+        }
+
+        if (ParryAction)
+        {
+            EIC->BindAction(ParryAction, ETriggerEvent::Started,   this, &APlayerCharacter::OnParryPressed);
+            EIC->BindAction(ParryAction, ETriggerEvent::Completed, this, &APlayerCharacter::OnParryReleased);
+        }
+
+        if (DodgeAction)
+        {
+            EIC->BindAction(DodgeAction, ETriggerEvent::Started, this, &APlayerCharacter::OnDodgePressed);
+        }
+    #pragma endregion
+        
+    #pragma region "LOCK TARGET BINDS"
+        if (LockToggleAction)
+        {
+            EIC->BindAction(LockToggleAction, ETriggerEvent::Started, this, &APlayerCharacter::OnLockToggle);
+        }
+        if (LockSwitchLeftAction)
+        {
+            EIC->BindAction(LockSwitchLeftAction, ETriggerEvent::Started, this, &APlayerCharacter::OnLockSwitchLeft);
+        }
+        if (LockSwitchRightAction)
+        {
+            EIC->BindAction(LockSwitchRightAction, ETriggerEvent::Started, this, &APlayerCharacter::OnLockSwitchRight);
+        }
+    #pragma endregion
     }
+    
 }
 
-void APlayerCharacter::OnForwardStarted(const FInputActionValue&)   { Vertical.OnPosStarted(GetWorld()->GetTimeSeconds()); }
-void APlayerCharacter::OnForwardCompleted(const FInputActionValue&){ Vertical.OnPosCompleted(); }
-void APlayerCharacter::OnBackStarted(const FInputActionValue&)      { Vertical.OnNegStarted(GetWorld()->GetTimeSeconds()); }
-void APlayerCharacter::OnBackCompleted(const FInputActionValue&)    { Vertical.OnNegCompleted(); }
-void APlayerCharacter::OnLeftStarted(const FInputActionValue&)      { Horizontal.OnNegStarted(GetWorld()->GetTimeSeconds()); }
-void APlayerCharacter::OnLeftCompleted(const FInputActionValue&)    { Horizontal.OnNegCompleted(); }
-void APlayerCharacter::OnRightStarted(const FInputActionValue&)     { Horizontal.OnPosStarted(GetWorld()->GetTimeSeconds()); }
-void APlayerCharacter::OnRightCompleted(const FInputActionValue&)   { Horizontal.OnPosCompleted(); }
-
+#pragma region "CAMERA INPUTS"
 void APlayerCharacter::Look(const FInputActionValue& Value)
 {
     const FVector2D LookAxis = Value.Get<FVector2D>();
@@ -139,20 +204,27 @@ void APlayerCharacter::Look(const FInputActionValue& Value)
         AddControllerPitchInput(LookAxis.Y);
     }
 }
+#pragma endregion
+
+#pragma region "MOVEMENT INPUTS"
+void APlayerCharacter::OnForwardStarted(const FInputActionValue&)   { Vertical.OnPosStarted(GetWorld()->GetTimeSeconds()); }
+void APlayerCharacter::OnForwardCompleted(const FInputActionValue&){ Vertical.OnPosCompleted(); }
+void APlayerCharacter::OnBackStarted(const FInputActionValue&)      { Vertical.OnNegStarted(GetWorld()->GetTimeSeconds()); }
+void APlayerCharacter::OnBackCompleted(const FInputActionValue&)    { Vertical.OnNegCompleted(); }
+void APlayerCharacter::OnLeftStarted(const FInputActionValue&)      { Horizontal.OnNegStarted(GetWorld()->GetTimeSeconds()); }
+void APlayerCharacter::OnLeftCompleted(const FInputActionValue&)    { Horizontal.OnNegCompleted(); }
+void APlayerCharacter::OnRightStarted(const FInputActionValue&)     { Horizontal.OnPosStarted(GetWorld()->GetTimeSeconds()); }
+void APlayerCharacter::OnRightCompleted(const FInputActionValue&)   { Horizontal.OnPosCompleted(); }
 
 void APlayerCharacter::StartSprint() { GetCharacterMovement()->MaxWalkSpeed = SprintSpeed; }
 void APlayerCharacter::StopSprint()  { GetCharacterMovement()->MaxWalkSpeed = WalkSpeed; }
 void APlayerCharacter::StartCrouch() { Crouch(); }
 void APlayerCharacter::StopCrouch()  { UnCrouch(); }
+#pragma endregion
 
-void APlayerCharacter::ToggleGlideMode()
-{
-    if (GliderComponent) GliderComponent->ToggleGliding();
-}
-void APlayerCharacter::ToggleDiveMode()
-{
-    if (GliderComponent) GliderComponent->ToggleDiving();
-}
+#pragma region "GLIDER INPUTS"
+void APlayerCharacter::ToggleGlideMode() { if (GliderComponent) GliderComponent->ToggleGliding(); }
+void APlayerCharacter::ToggleDiveMode() { if (GliderComponent) GliderComponent->ToggleDiving(); }
 
 void APlayerCharacter::AlignToCamera()
 {
@@ -163,28 +235,19 @@ void APlayerCharacter::AlignToCamera()
     }
 }
 
-bool APlayerCharacter::IsInSpecialMode() const
-{
-    return (GliderComponent && GliderComponent->IsGliding());
-}
+bool APlayerCharacter::IsInSpecialMode() const { return (GliderComponent && GliderComponent->IsGliding()); }
+#pragma endregion
 
-void APlayerCharacter::Input_SelectNext()
-{
-    if (InventoryComponent) { InventoryComponent->SelectNext(); }
-}
-void APlayerCharacter::Input_SelectPrev()
-{
-    if (InventoryComponent) { InventoryComponent->SelectPrevious(); }
-}
-void APlayerCharacter::Input_UseItem()
-{
-    if (InventoryComponent) { InventoryComponent->UseSelected(); }
-}
-void APlayerCharacter::Input_DropItem()
-{
-    if (InventoryComponent) { InventoryComponent->DropSelected(true, 1); }
-}
+#pragma region "INVENTORY INPUTS"
+// INVENTORY INPUTS
+void APlayerCharacter::Input_SelectNext() { if (InventoryComponent) { InventoryComponent->SelectNext(); } }
+void APlayerCharacter::Input_SelectPrev() { if (InventoryComponent) { InventoryComponent->SelectPrevious(); } }
+void APlayerCharacter::Input_UseItem() { if (InventoryComponent) { InventoryComponent->UseSelected(); } }
+void APlayerCharacter::Input_DropItem() { if (InventoryComponent) { InventoryComponent->DropSelected(true, 1); } }
+#pragma endregion
 
+#pragma region "INTERACTION INPUT"
+// INTERACTION INPUT
 void APlayerCharacter::Input_Interact()
 {
     UE_LOG(LogTemp, Warning, TEXT("Interact pressed"));
@@ -195,3 +258,98 @@ void APlayerCharacter::Input_Interact()
     }
     InteractorComponent->TryInteract();
 }
+#pragma endregion
+
+#pragma region "COMBAT INPUTS"
+// Light attack: try primary if idle, else request combo advance during combo window
+void APlayerCharacter::OnAttackLightPressed()
+{
+    if (!CombatComponent) return;
+
+    if (CombatComponent->IsInAttackWindow())
+    {
+        // Buffer the next step while the combo window is open
+        CombatComponent->RequestComboAdvance();
+    }
+    else
+    {
+        // Start the default primary attack (first in the array)
+        CombatComponent->TryAttackPrimary();
+    }
+}
+
+void APlayerCharacter::OnAttackLightReleased()
+{
+    
+}
+
+// Heavy charge attack: use an explicit attack id, for example "Heavy"
+void APlayerCharacter::OnAttackHeavyPressed()
+{
+    if (!CombatComponent) return;
+    // The heavy attack spec should have Charge.bChargeable = true in the component settings
+    CombatComponent->TryAttackById(FName("Heavy"));
+}
+
+// Release the charge and execute at current level
+void APlayerCharacter::OnAttackHeavyReleased()
+{
+    if (!CombatComponent) return;
+    // EndCharge(false) will compute level and release.
+    CombatComponent->EndCharge(false);
+}
+
+// Cancel the charge without releasing
+void APlayerCharacter::OnAttackHeavyCanceled()
+{
+    if (!CombatComponent) return;
+    CombatComponent->EndCharge(true);
+}
+
+void APlayerCharacter::OnParryPressed()
+{
+    if (!CombatComponent) return;
+    CombatComponent->SetParryHeld(true);
+}
+
+void APlayerCharacter::OnParryReleased()
+{
+    if (!CombatComponent) return;
+    CombatComponent->SetParryHeld(false);
+}
+
+void APlayerCharacter::OnDodgePressed()
+{
+    // Have to play a dodge montage that contains a notify to call StartDodgeIFrames
+    if (CombatComponent)
+    {
+        CombatComponent->StartDodgeIFrames(-1.f);
+    }
+}
+#pragma endregion
+
+#pragma region "LOCK TARGET INPUTS"
+void APlayerCharacter::OnLockToggle()
+{
+    if (LockTargetComponent)
+    {
+        LockTargetComponent->ToggleLock(nullptr);
+    }
+}
+
+void APlayerCharacter::OnLockSwitchLeft()
+{
+    if (LockTargetComponent)
+    {
+        LockTargetComponent->SwitchTarget(false);
+    }
+}
+
+void APlayerCharacter::OnLockSwitchRight()
+{
+    if (LockTargetComponent)
+    {
+        LockTargetComponent->SwitchTarget(true);
+    }
+}
+#pragma endregion
--- Etheria/Source/Etheria/Public/Characters/Players/PlayerCharacter.h
+++ Etheria/Source/Etheria/Public/Characters/Players/PlayerCharacter.h
@@ -1,7 +1,7 @@
 /**
  * Etheria's End Project, 2025
  * Created by: Zhailendra
- * Last Updated by: Zhailendra
+ * Last Updated by: 0nnen
  * Class: PlayerCharacter - Header
  */
 
@@ -19,6 +19,8 @@ class UInputAction;
 class UGliderComponent;
 class UInventoryComponent;
 class UInteractorComponent;
+class UCombatComponent;
+class ULockTargetComponent;
 
 USTRUCT()
 struct FAxisPressState
@@ -85,7 +87,8 @@ protected:
     UFUNCTION() void Input_UseItem();
     UFUNCTION() void Input_DropItem();
     UFUNCTION() void Input_Interact();
-
+    
+#pragma region "COMPONENTS"
     // === CAMERA COMPONENTS ===
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera Components")
     USpringArmComponent* CameraBoom;
@@ -93,27 +96,52 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera Components")
     UCameraComponent* FollowCamera;
 
-    // === GLIDER ===
+    // === GLIDER COMPONENT ===
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Glider")
     UGliderComponent* GliderComponent;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Glider|Visual")
     UStaticMeshComponent* GliderVisual;
 
-    // === PLAYER COMPONENTS ===
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Player Components", meta=(AllowPrivateAccess="true"))
+    // === INVENTORY COMPONENTS ===
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Inventory Component", meta=(AllowPrivateAccess="true"))
     UInventoryComponent* InventoryComponent;
 
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Player Components", meta=(AllowPrivateAccess="true"))
+    // === Interactor COMPONENTS ===
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Interactor Component", meta=(AllowPrivateAccess="true"))
     UInteractorComponent* InteractorComponent;
 
-    // === INPUT ===
+    // === COMBAT COMPONENTS ===
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Combat Components", meta=(AllowPrivateAccess="true"))
+    UCombatComponent* CombatComponent;
+
+    // === LOCK TARGET COMPONENTS ===
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Lock Target Components", meta=(AllowPrivateAccess="true"))
+    ULockTargetComponent* LockTargetComponent;
+#pragma endregion
+    
+#pragma region "INPUTS"
+    // --===-- INPUTS --===--
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
     UInputMappingContext* PlayerContext;
 
+    // === CAMERA ACTION ===
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
     UInputAction* LookAction;
+    
+    // === MOVEMENTS ACTIONS ===
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
+    UInputAction* ForwardAction;
 
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
+    UInputAction* BackAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
+    UInputAction* LeftAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
+    UInputAction* RightAction;
+    
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
     UInputAction* JumpAction;
 
@@ -123,12 +151,14 @@ protected:
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
     UInputAction* SprintAction;
 
+    // === GLIDER ACTIONS ===
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Input")
     UInputAction* GliderAction;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player Input")
     UInputAction* DiveAction;
 
+    // === INVENTORY ACTIONS ===
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Input")
     UInputAction* NextItemAction;
 
@@ -142,21 +172,33 @@ protected:
     UInputAction* DropItemAction;
 
     UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Input")
-    UInputAction* InteractAction; // (Pick up)
+    UInputAction* InteractAction;
+    
+    // === COMBAT ACTIONS ===
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Input")
+    UInputAction* AttackLightAction;
 
-    // 4 actions séparées
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
-    UInputAction* ForwardAction;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Input")
+    UInputAction* AttackHeavyAction; // used for charge attacks
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
-    UInputAction* BackAction;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Input")
+    UInputAction* ParryAction;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
-    UInputAction* LeftAction;
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Input")
+    UInputAction* DodgeAction;
 
-    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Player Input")
-    UInputAction* RightAction;
+    // === LOCK TARGET ACTIONS ===
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Input")
+    UInputAction* LockToggleAction;
 
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Input")
+    UInputAction* LockSwitchLeftAction;
+
+    UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Player Input")
+    UInputAction* LockSwitchRightAction;
+
+#pragma endregion
+    
     // === MOVEMENT SPEEDS ===
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
     float WalkSpeed = 600.f;
@@ -165,7 +207,7 @@ protected:
     float SprintSpeed = 900.f;
 
 private:
-    // États d’axes
+    // STATES AXIS
     FAxisPressState Horizontal;
     FAxisPressState Vertical;
 
@@ -173,7 +215,8 @@ private:
     bool bIsLookingAround = false;
     float TimeSinceLastLook = 0.f;
 
-    // Handlers
+#pragma region "HANDLERS"
+    // === MOVEMENTS HANDLERS ===
     void OnForwardStarted(const FInputActionValue& Value);
     void OnForwardCompleted(const FInputActionValue& Value);
     void OnBackStarted(const FInputActionValue& Value);
@@ -182,4 +225,20 @@ private:
     void OnLeftCompleted(const FInputActionValue& Value);
     void OnRightStarted(const FInputActionValue& Value);
     void OnRightCompleted(const FInputActionValue& Value);
+    
+    // === COMBAT HANDLERS ===
+    void OnAttackLightPressed();
+    void OnAttackLightReleased();
+    void OnAttackHeavyPressed();
+    void OnAttackHeavyReleased();   // release the charge at current level
+    void OnAttackHeavyCanceled();   // cancel the charge if needed
+    void OnParryPressed();
+    void OnParryReleased();
+    void OnDodgePressed();
+    void OnLockToggle();
+    
+    // === LOCK TARGET HANDLERS ===
+    void OnLockSwitchLeft();
+    void OnLockSwitchRight();
+#pragma endregion
 };
